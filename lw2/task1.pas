4d18055c53f2bb76d79dc8f3a554e9a726b1575f
dima1593587 <79845080+dima1593587@users.noreply.github.com>
2021-03-01 15:35:21 +0300

    Update task1.pas

--- lw2/task1.pas
+++ lw2/task1.pas
@@ -1,39 +1,12 @@
 PROGRAM PrintEnv(INPUT, OUTPUT);
-VAR
-  IP: STRING;
-  Count: INTEGER;
-BEGIN {PrintEnv}
-  READLN(IP);
-  Count := 0 ;
-  WHILE POS('.', IP) <> 0 
-  DO
-    BEGIN
-       IF (VAL(COPY(IP, 1, POS('.', IP) - 1)) > 256) OR (VAL(COPY(IP, 1, POS('.', IP) - 1)) < 0)
-       THEN
-         BEGIN
-           Count := -1;
-           BREAK
-         END
-       ELSE
-         BEGIN
-           DELETE(IP, 1, POS('.', IP));
-           Count := Count + 1 
-         END   
-    END;
-  IF (VAL(IP) > 256) OR (VAL(IP) < 0)
-  THEN
-    BEGIN
-      Count := -1;
-    END
-  ELSE
-    BEGIN
-      Count := Count + 1 
-    END;
-  IF Count = 4 
-  THEN 
-    WRITELN('Good')
-  ELSE
-    WRITELN('Bad')   
-
-END. {PrintEnv}
+USES 
+  DOS;
+BEGIN {PrintHello}
+  WRITELN;
+  WRITELN('REQUEST_METHOD:', GetEnv('REQUEST_METHOD'));
+  WRITELN('QUERY_STRING:', GetEnv('QUERY_STRING'));
+  WRITELN('CONTENT_LENGTH:', GetEnv('CONTENT_LENGTH'));
+  WRITELN('HTTP_USER_AGENT:', GetEnv('HTTP_USER_AGENT'));
+  WRITELN('HTTP_HOST:', GetEnv('HTTP_HOST'));
+END. {PrintHello}
 
